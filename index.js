eb47e8cf675c95cfd007789116961ce5df2b3d08
varshateeratipally <139103379+varshateeratipally@users.noreply.github.com>
2024-08-15 13:29:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,20 +3,13 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3060;
-//Step 3 - Make the styling show up.
-//Hint 1: CSS files are static files!
-//Hint 2: The header and footer are partials.
-//Hint 3: Add the CSS link in header.ejs
-
-//Step 4 - Add a dynamic year to the footer.
-//Hint: Google to find out how to get the current year using JS.
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get("/", (req, res) => {
-  //Step 1 - Make the get route work and render the index.ejs file.
+ 
   res.render("index.ejs");
 });
 
@@ -29,13 +22,7 @@ app.post("/submit", (req, res) => {
     nouns:rnoun,
 })
 
-  //Step 2 - Make the generate name functionality work
-  //Hint: When the "Generate Name" button in index.ejs is clicked, it should hit up this route.
-  //Then:
-  //1. You should randomly pick an adjective from the const "adj" and a noun from const "noun",
-  //scroll down to see the two arrays.
-  //2. Send the index.ejs as a response and add the adjective and noun to the res.render
-  //3. Test to make sure that the random words display in the h1 element in index.ejs
+ 
 });
 
 app.listen(port, () => {
@@ -797,8 +784,6 @@ const adj = [
   "natural",
   "naughty",
   "nautical",
-  "near",
-  "neat",
   "necessary",
   "needy",
   "negative",
@@ -806,10 +791,6 @@ const adj = [
   "negligible",
   "neighboring",
   "nervous",
-  "new",
-  "next",
-  "nice",
-  "nifty",
   "nimble",
   "nippy",
   "nocturnal",
@@ -1685,10 +1666,6 @@ const noun = [
   "bath",
   "bather",
   "bathhouse",
-  "bathrobe",
-  "bathroom",
-  "bathtub",
-  "batter",
   "battery",
   "batting",
   "battle",
